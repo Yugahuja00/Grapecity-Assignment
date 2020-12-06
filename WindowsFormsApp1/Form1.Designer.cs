8a153898f8901d1cee0f21a9fc38b8939843c0d6
Yug Ahuja <49425592+Yugahuja00@users.noreply.github.com>
2020-12-06 21:31:30 +0530

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtbill = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtt1 = new System.Windows.Forms.TextBox();
+            this.txtt2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.person = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txttip = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(409, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bill";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tip%";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 327);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Number of People";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(370, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tip";
+            // 
+            // txtbill
+            // 
+            this.txtbill.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbill.Location = new System.Drawing.Point(44, 77);
+            this.txtbill.Name = "txtbill";
+            this.txtbill.Size = new System.Drawing.Size(145, 34);
+            this.txtbill.TabIndex = 5;
+            this.txtbill.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(370, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Total";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtt1
+            // 
+            this.txtt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtt1.Location = new System.Drawing.Point(517, 71);
+            this.txtt1.Name = "txtt1";
+            this.txtt1.Size = new System.Drawing.Size(145, 34);
+            this.txtt1.TabIndex = 9;
+            this.txtt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtt1.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // txtt2
+            // 
+            this.txtt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtt2.Location = new System.Drawing.Point(517, 186);
+            this.txtt2.Name = "txtt2";
+            this.txtt2.Size = new System.Drawing.Size(145, 41);
+            this.txtt2.TabIndex = 10;
+            this.txtt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtt2.TextChanged += new System.EventHandler(this.t2_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(371, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Per Person";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(368, 214);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Per Person";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(228, 355);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 32);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // person
+            // 
+            this.person.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.person.Location = new System.Drawing.Point(77, 355);
+            this.person.Name = "person";
+            this.person.Size = new System.Drawing.Size(145, 30);
+            this.person.TabIndex = 17;
+            this.person.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.person.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(39, 355);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(33, 32);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(39, 214);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(33, 32);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(228, 215);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(33, 32);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "+";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // txttip
+            // 
+            this.txttip.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttip.Location = new System.Drawing.Point(77, 214);
+            this.txttip.Name = "txttip";
+            this.txttip.Size = new System.Drawing.Size(145, 30);
+            this.txttip.TabIndex = 21;
+            this.txttip.Text = "        %";
+            this.txttip.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txttip.TextChanged += new System.EventHandler(this.txttip_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 450);
+            this.Controls.Add(this.txttip);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.person);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtt2);
+            this.Controls.Add(this.txtt1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtbill);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtbill;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtt1;
+        private System.Windows.Forms.TextBox txtt2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox person;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txttip;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static int i, j;
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            double bill, tip, people,t1, t2;
+            char c = '%';
+            txttip.Text = txttip.Text.Replace(c.ToString(), String.Empty);
+            bill = Convert.ToDouble(txtbill.Text);
+            tip = Convert.ToDouble(txttip.Text);
+            people = Convert.ToDouble(person.Text);
+            if (tip < 0 || people <= 0 || bill <= 0)
+            {
+                string message = "Please enter a Valid Input";
+                string title = "Invalid Input!";
+                MessageBox.Show(message, title);
+                string msg = "Do you want to abort this operation?";
+                string tt = "Close Window";
+                MessageBoxButtons buttons = MessageBoxButtons.AbortRetryIgnore;
+                DialogResult result = MessageBox.Show(msg, tt, buttons, MessageBoxIcon.Warning);
+                if (result == DialogResult.Abort)
+                {
+                    this.Close();
+                }
+                else if (result == DialogResult.Retry)
+                {
+                    Form1 NewForm = new Form1();
+                    NewForm.Show();
+                    this.Dispose(false);
+                }
+            }
+            t1 =  (bill/people) * tip/100;
+            t2 = (bill / people) + t1;
+            t1=Math.Round(t1, 2);
+            t2=Math.Round(t2, 2);
+            txttip.Text = tip.ToString() + "%";
+            txtt1.Text = "$" + t1.ToString();
+            txtt2.Text = "$" +t2.ToString();
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+     
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tip_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void t2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            j++;
+            person.Text = j.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            i--;
+            txttip.Text = i.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            i++;
+            txttip.Text = i.ToString();
+        }
+
+        private void txttip_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            j--;
+            person.Text = j.ToString();
+        }
+    }
+}
